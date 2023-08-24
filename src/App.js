8ddb0edf9b777cc54f9065704a1cce160e55d445
Vladyslav Razvodovskyi <v.razvodovskyi@dxc.com>
2023-08-25 02:55:21 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {Link, Route, Routes} from "react-router-dom";
+import JOP from "./JOP";
+
+var counter = 0;
+var names = ["Влад", "Ваня", "Юля", "Гарик", "Мотя"]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [c, setC] = useState()
+   function count() { 
+      counter++;
+      counter = counter % names.length;
+      setC(counter);
+   }
+
+   return (
+      <>
+         <Link to="/JOP" onClick={count}>Click me</Link>
+         <Routes>
+            <Route path="/JOP" element={<JOP name={names[c]}/>}/>
+         </Routes>
+      </>
+   );
 }
 
 export default App;
---
+++ src/JOP.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function JOP({name="vlad"}) { 
+    return (
+        <span>{name}</span>
+    );
+}
